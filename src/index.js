c1b4877977d7f3ae6644f463dfa6d2fc9121eec3
amjadT1000 <Amjad.Athathi.ruh.js@tuwaiq.edu.sa>
2021-10-26 09:25:44 +0300

    init commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useDebugValue } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import HelloMessage from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const tweets = {
+  Max: {
+    name: "Max",
+    image: "https://miro.medium.com/max/1225/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+    tweets: "Be yourself; everyone else is already taken.",
+  },
+  Alix: {
+    name: "Alix",
+    image: "https://miro.medium.com/max/1225/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+    tweets: "So many books, so little time.",
+  },
+  Tom: {
+    name: "Tom",
+    image: "https://miro.medium.com/max/1225/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+    tweets: "You only live once, but if you do it right, once is enough.",
+  },
+};
+console.log(tweets);
+const tabs = [
+  <HelloMessage
+    image={tweets.Max.image}
+    title={tweets.Max.name}
+    dec={tweets.Max.tweets}
+  />,
+  <HelloMessage
+    image={tweets.Alix.image}
+    title={tweets.Alix.name}
+    dec={tweets.Alix.tweets}
+  />,
+  <HelloMessage
+    image={tweets.Tom.image}
+    title={tweets.Tom.name}
+    dec={tweets.Tom.tweets}
+  />,
+];
+
+ReactDOM.render(tabs, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
